a231a1330234bd5781a5589ac0e1db2929944a11
Angel Alvarado <aata030115@gs.utm.mx>
2024-05-28 18:50:46 -0600

    Intento 3

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -1,15 +1,15 @@
 export default {
   database: {
-    host: "ls-d78700e295fdf0285aaa47044611235e252b3034.cl8ceoyk4wgx.us-east-2.rds.amazonaws.com ",
+    host: "ls-d78700e295fdf0285aaa47044611235e252b3034.cl8ceoyk4wgx.us-east-2.rds.amazonaws.com",
     port: 3306,
     user: "dbmasteruser",
     password: "4w||m+pUG%|x_`r:17Udw1wl&GV#ekEE",
-    database: "ambarstore",
+    database: "pruebapracticas",
   },
-  //database: {
-  //  host: "localhost",
-  //  user: "root",
-  //  password: "",
-  //  database: "pruebapracticas",
-  //},
+  /*database:{
+      host:'localhost',
+      user:'root',
+      password:'',
+      database:'ambarstore'
+    }*/
 };
